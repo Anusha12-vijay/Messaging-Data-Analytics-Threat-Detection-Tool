778211f9447bea19724f5edd0426a37cd5f714d2
Anusha12-vijay <anushavijay1202@gmail.com>
2026-01-11 04:19:47 +0530

    Added the title and the ability for the text file to upload in the streamlit website

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import streamlit as st
 import re
+import preprocessor
+st.sidebar.title("Messaging Data Analytics & Threat Detection Tool")
+
+uploaded_file =st.sidebar.file_uploader("Choose the chat file")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    # st.text(data)
+    df=preprocessor.preprocess(data)
+
+    st.dataframe(df)
--- preprocessor.py
+++ preprocessor.py
@@ -1,3 +1,6 @@
+import re
+import pandas as pd
+
 def preprocess(data):
     pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
     messages = re.split(pattern, data)[1:]
@@ -31,6 +34,8 @@ def preprocess(data):
         df['day'] = df['date'].dt.day
         df['hour'] = df['date'].dt.hour
         df['minute'] = df['date'].dt.minute
+
+        return df
 
 
 
