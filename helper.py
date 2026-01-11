556b3d1f5a95706517a5a20677eb2c1efa7c08b9
Anusha12-vijay <anushavijay1202@gmail.com>
2026-01-11 19:51:28 +0530

    Now can calculate the total number of messages and total number of words used

---
+++ helper.py
@@ -0,0 +1,20 @@
+def fetch_stats(selected_user,df):
+    if selected_user == "Overall Group":
+        #Number of messages
+        num_messages=df.shape[0]
+
+        #Number of words
+        words=[]
+        for message in df['message']:
+            words.extend(message.split())
+
+        return num_messages,len(words)
+    else:
+        new_df= df[df['user'] == selected_user]
+        num_messages =new_df.shape[0]
+        words = []
+        for message in df['message']:
+            words.extend(message.split())
+        return num_messages,len(words)
+
+
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import re
 import preprocessor
+import helper
+
 st.sidebar.title("Messaging Data Analytics & Threat Detection Tool")
 
 uploaded_file =st.sidebar.file_uploader("Choose the chat file")
@@ -18,4 +20,16 @@ if uploaded_file is not None:
     user_list.sort()
     user_list.insert(0,"Overall Group")
 
-    st.sidebar.selectbox("Show analysis for ",user_list)
+    selected_user=st.sidebar.selectbox("Show analysis for ",user_list)
+
+    if st.sidebar.button("Show Analysis"):
+        num_messages,words =helper.fetch_stats(selected_user,df)
+        col1 ,col2,col3,col4 = st.columns(4)
+        with col1:
+            st.header("Total Messages")
+            st.title(num_messages)
+        with col2:
+            st.header("Total Words")
+            st.title(words)
+
+
